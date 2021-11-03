339368893e26b0518ef840e302f59b497b86e9bd
elbub1979 <ilya.leuh09@gmail.com>
2021-11-04 00:40:35 +0300

    "Make Depot with Products"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'ad8f5625ab1a770e9ac2d0184ae45464fdd026024c6ec2edd431a96a197840bad33ba3b01feb6177674fb49f0d7bcafdccb960e149007debdd67466fe64fb2eb'
