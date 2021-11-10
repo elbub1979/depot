d6174e23acd2ed938af9b24a31f2d8e85b76dd04
elbub1979 <ilya.leuh09@gmail.com>
2021-11-10 21:54:28 +0300

    "Part 10"

--- db/migrate/20211109195126_add_price_in_line_items.rb
+++ db/migrate/20211109195126_add_price_in_line_items.rb
@@ -1,9 +1,19 @@
 class AddPriceInLineItems < ActiveRecord::Migration
-  def add_price
+  def up
     Cart.all.each do |cart|
       cart.line_items.each do |line_item|
-        product_price = Product.find(line_item.product_id).price.to_f
+        product_price = Product.find(line_item.product_id).price
         line_item.product_price = product_price
+        line_item.save!
+      end
+    end
+  end
+
+  def down
+    Cart.all.each do |cart|
+      cart.line_items.each do |line_item|
+        line_item.product_price = nil
+        line_item.save!
       end
     end
   end
